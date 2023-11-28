c145b06df0adabe41e7e7ee2f8805d545007fee1
wnsdud4206 <66582964+wnsdud4206@users.noreply.github.com>
2023-11-28 18:30:09 +0900

    20231128 포트폴리오 리뷰 (boards 수정 1편) 0:11~

---
+++ pages/section09/09-04-boards/new/index.js
@@ -0,0 +1,5 @@
+import BoardWrite from "../../../../src/components/units/board/09-write2/BoardWrite.container";
+
+export default function GraphqlMutationPage() {
+  return <BoardWrite isEdit={false} />;
+}
---
+++ src/components/units/board/09-write2/BoardWrite.presenter.js
@@ -0,0 +1,38 @@
+import { RedInput, BlueButton } from "./BoardWrite.styles";
+
+export default function BoardWriteUI({
+  onChangeInp,
+  onClickUpdate,
+  onClickSubmit,
+  isEdit,
+  data,
+}) {
+  return (
+    <div>
+      작성자:{" "}
+      <RedInput
+        id="writer"
+        type="text"
+        defaultValue={data?.fetchBoard.writer}
+        onChange={onChangeInp}
+      />
+      제목:{" "}
+      <input
+        id="title"
+        type="text"
+        defaultValue={data?.fetchBoard.title}
+        onChange={onChangeInp}
+      />
+      내용:{" "}
+      <input
+        id="contents"
+        type="text"
+        defaultValue={data?.fetchBoard.contents}
+        onChange={onChangeInp}
+      />
+      <BlueButton onClick={isEdit ? onClickUpdate : onClickSubmit}>
+        {isEdit ? "수정" : "등록"}하기
+      </BlueButton>
+    </div>
+  );
+}
