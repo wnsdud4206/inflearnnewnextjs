19aed19c5fdb100262343fcae7d1222ab9edb967
wnsdud4206 <66582964+wnsdud4206@users.noreply.github.com>
2024-01-06 17:55:47 +0900

    20240106 섹션 17 포트폴리오 리뷰 23:32~

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
+import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
+import { globalStyles } from "../src/commons/types/generated/styles/globalStyles";
 import ApolloSetting from "../src/components/commons/apollo";
 import Layout from "../src/components/commons/layout";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <ApolloSetting>
+      <>
+      <Global styles={globalStyles}/>
       <Layout>
         <Component {...pageProps} />
-      </Layout>
+      </Layout></>
     </ApolloSetting>
   );
 }
