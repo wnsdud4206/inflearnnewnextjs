067619a56c1d717d0eac8bc6f849d65638dfac52
wnsdud4206 <66582964+wnsdud4206@users.noreply.github.com>
2024-01-17 18:58:06 +0900

    20240117 16-02-infinite-scroller 20:24~

---
+++ pages/section16/16-02-infinite-scroller/index.tsx
@@ -0,0 +1,35 @@
+import { gql, useQuery } from "@apollo/client";
+import InfiniteScroll from 'react-infinite-scroller';
+import type {
+  IQuery,
+  IQueryFetchBoardsArgs,
+} from "../../../src/commons/types/generated/types";
+
+const FETCH_BOARDS = gql`
+  query fetchBoards($page: Int) {
+    fetchBoards(page: $page) {
+      _id
+      writer
+      title
+      contents
+    }
+  }
+`;
+
+export default function StaticRoutingBoardMovedPage(): JSX.Element {
+  const { data } = useQuery<
+    Pick<IQuery, "fetchBoards">,
+    IQueryFetchBoardsArgs
+  >(FETCH_BOARDS);
+
+  return (
+    <div>
+      {data?.fetchBoards.map((el) => (
+        <div key={el._id}>
+          <span style={{ margin: "10px" }}>{el.title}</span>
+          <span style={{ margin: "10px" }}>{el.writer}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
