d209ecfd35adaeaa156ab45bdcda921c5e8c83f6
wnsdud4206 <66582964+wnsdud4206@users.noreply.github.com>
2024-01-22 18:04:15 +0900

    20240122 섹션 19 포트폴리오 리뷰 35:45~

--- pages/section16/16-03-inputs-refectoring/01-after.tsx
+++ pages/section16/16-03-inputs-refectoring/01-after.tsx
@@ -23,27 +23,31 @@ export default function GraphqlMutationPage(): JSX.Element {
       return;
     }
     const result = await myFunc({
-      variables: {
-        writer,
-        title,
-        contents,
-      },
+      // variables: {
+      //   writer,
+      //   title,
+      //   contents,
+      // },
+      // 이것도 아래처럼 간단하게 spread를 사용해서 한 줄로
+      variables: { ...inputs },
     });
     console.log(result);
   };
 
-  const onChangeInp = ({
+  const onChangeInputs = ({
     target: { id, value },
   }: ChangeEvent<HTMLInputElement>): void => {
-    const newInputs = { ...inputs, [id]: value };
-    setInputs(newInputs);
+    // const newInputs = { ...inputs, [id]: value };
+    // setInputs(newInputs);
+    // 위처럼 기존 state인 inputs를 사용하거나 아래처럼 prev를 사용해도 상관없지만 유지보수나 안정성 등 여러가지 측면에서 기존 데이터인 prev를 사용하는 편이 더 좋다.
+    setInputs((prev) => ({ ...prev, [id]: value }));
   };
 
   return (
     <div>
-      작성자: <input id="writer" type="text" onChange={onChangeInp} />
-      제목: <input id="title" type="text" onChange={onChangeInp} />
-      내용: <input id="contents" type="text" onChange={onChangeInp} />
+      작성자: <input id="writer" type="text" onChange={onChangeInputs} />
+      제목: <input id="title" type="text" onChange={onChangeInputs} />
+      내용: <input id="contents" type="text" onChange={onChangeInputs} />
       <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
     </div>
   );
