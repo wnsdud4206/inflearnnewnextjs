b346db2fcfa56c682bc0c0ffed9f2d776b2eb986
wnsdud4206 <66582964+wnsdud4206@users.noreply.github.com>
2024-03-07 18:18:05 +0900

    20240307 23-로그인 처리 과정 19:59~

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,21 @@
 import { Global } from "@emotion/react";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 import { globalStyles } from "../src/commons/styles/globalStyles";
 import ApolloSetting from "../src/components/commons/apollo";
 import Layout from "../src/components/commons/layout";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ApolloSetting>
-      <>
-        <Global styles={globalStyles}/>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </>
-    </ApolloSetting>
+    <RecoilRoot>
+      <ApolloSetting>
+        <>
+          <Global styles={globalStyles} />
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </>
+      </ApolloSetting>
+    </RecoilRoot>
   );
 }
---
+++ src/commons/stores/index.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const isEditState = atom({
+  key: "isEditState",  // 실제 변수는 이 이름이고 useRecoilState에서 isEdit이라는 다른 이름이로 사용하는 것이다.
+  default: true,  // 초기값, 기본값
+});
