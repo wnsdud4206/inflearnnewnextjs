6d15275ba21f188dc5e15e20e86cc4a19b42e448
wnsdud4206 <66582964+wnsdud4206@users.noreply.github.com>
2024-03-13 18:40:34 +0900

    20240313 23-02-login-localstorage / login-localstorage-success 0:06~

--- pages/section23/23-01-login/index.tsx
+++ pages/section23/23-01-login/index.tsx
@@ -1,5 +1,8 @@
 import { gql, useMutation } from "@apollo/client";
 import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../../src/commons/stores";
+import { useRouter } from "next/router";
 import type { ChangeEvent } from "react";
 import type {
   IMutation,
@@ -15,11 +18,16 @@ const LOGIN_USER = gql`
 `;
 
 export default function LoginPage(): JSX.Element {
+  const router = useRouter();
   const [input, setInput] = useState({ email: "", password: "" });
   const [loginUser] = useMutation<
     Pick<IMutation, "loginUser">,
     IMutationLoginUserArgs
   >(LOGIN_USER);
+
+  // accessToken state가 필요없다면 지워되는데 콤마(,)는 지우면 안된다.(useState도?) 왜 콤마는 지우면 안되나면 나중에 구조분해할당을 배울 때 이해할 수 있다.
+  // *recoil 공식 사이트의 문서를 보니까 useSetRecoilState로 accessTokenState를 넘겨주고 아래처럼 굳이 accessToken state를 지울 필요없이 setAccessToken만 사용할 수 있다는데?, state를 사용할 때는 useRecoilValue(accessToken) 이런식으로, 일단 아래처럼 state를 지우고 사용하는 원리를 배우기 위해 그대로 진행
+  const [, setAccessToken] = useRecoilState(accessTokenState);
 
   const onChangeInput = ({
     currentTarget: { id, value },
@@ -31,17 +39,40 @@ export default function LoginPage(): JSX.Element {
   };
 
   const onClickLogin = async (): Promise<void> => {
-    const { email, password } = input;
-    const result = await loginUser({
-      variables: {
-        email,
-        password,
-      },
-    });
+    try {
+      // 1. 로그인 mutation 날려서 accessToken 받아오기
+      const { email, password } = input;
+      const result = await loginUser({
+        variables: {
+          email,
+          password,
+        },
+      });
 
-    console.log(result.data?.loginUser.accessToken); // accessToken
+      console.log(result.data?.loginUser.accessToken); // accessToken
 
-    const { accessToken } = result.data?.loginUser;
+      const accessToken = result.data?.loginUser.accessToken;
+
+      // 2. 받아온 accessToken을 globalState에 저장하기
+      if (accessToken === undefined) {
+        alert("로그인 실패");
+        return;
+      }
+      setAccessToken(accessToken);
+
+      // 로그인 성공 페이지로 이동하기
+      void router.push("/section23/23-01-login-success");
+    } catch (error) {
+      // 그냥 error.message로 사용하면 error에 message가 있는지 모르겠다는 문제가 생겨서 if문으로 처리, error가 Error에서 파생된 건지
+      /* ex.
+        const data = new Date();
+        console.log(data instanceof Date);  // true
+      */
+      if (error instanceof Error) {
+        alert(error.message);
+        console.error(error.message);
+      }
+    }
   };
 
   return (
--- src/components/commons/apollo/index.tsx
+++ src/components/commons/apollo/index.tsx
@@ -8,17 +8,30 @@ import {
 import { createUploadLink } from "apollo-upload-client";
 // 강의에서는 위 처럼 해줬는데 버전이 달라져서 그런가 최신버전은 아래처럼 해줘야 error가 안생김
 // import createUploadLink from "apollo-upload-client/createUploadLink.mjs";
+import { useRecoilState } from "recoil";
+import { accessTokenState } from "../../../commons/stores";
 
 // 보통 이런 상수(변하지 않는 값)는 대문자로 지어주는 것이 좋다.
 const GLOBAL_STATE = new InMemoryCache();
+/*
+최신 트렌드는 globalState를 2개 사용
+서버데이터(백엔드에서 받아온 데이터) + 프론트데이터(프론트에서 관리하는 데이터)
+REST-API의 경우: ReactQuery + recoil
+GraphQL-API의 경우: ApolloClient + recoil
+즉, 위에 GLOBAL_STATE = new InMemoryCache()는 백엔드에서 받아온 데이터를 객체형태로 만들어서 new InMemoryCache()에 저장해두는 것
+*/
 
 interface IApolloSettingProps {
   children: JSX.Element;
 }
 
 export default function ApolloSetting(props: IApolloSettingProps): JSX.Element {
+  const [accessToken] = useRecoilState(accessTokenState);
+
   const uploadLink = createUploadLink({
     uri: "https://backend-practice.codebootcamp.co.kr/graphql",
+    headers: { Authorization: `Bearer ${accessToken}` },
+    // 이제 어느 컴포넌트든 API 요청을 보내면 accessToken이 담긴다.
   });
   /*
   const errorLink
